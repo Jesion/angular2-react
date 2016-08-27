2078d96827a34bb17b69ce467985e4f01653d4b0
Robert Jesionek <yesionuk@gmail.com>
2016-08-28 01:27:16 +0200

    list on react component

--- src/app/components/react/child/child.tsx
+++ src/app/components/react/child/child.tsx
@@ -11,14 +11,22 @@ var ReactChildComponent = React.createClass<ReactChildComponentViewProps, any>({
     getInitialState: function() {
          this.props.store.store.subscribe(() => {
             console.log('React captured store changed: ' + this.props.store.store.getState().length);
-            this.setState({ 'count': this.props.store.store.getState().length });      
+            this.setState({ 'count': this.props.store.store.getState().length, 'elements' : this.props.store.store.getState() });      
         });
-        return { 'title': this.props.title, 'count':  this.props.store.store.getState().length }       
+        return { 'title': this.props.title, 'count':  this.props.store.store.getState().length, 'elements': this.props.store.store.getState() }       
     },
 
     render: function() {
+        var elements = this.state.elements;
         return(
-           <div>{this.state.title + ' Current state: ' +  this.state.count}</div>
+           <div>
+            <label>{this.state.title + ' Current state: ' +  this.state.count}</label>
+            <ul>
+                {elements.map(function(listValue){
+                    return <li>{listValue}</li>;
+                })}
+            </ul>
+           </div>
         );
     }
 });
