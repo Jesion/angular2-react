19e0d74d9183398161ec3bb29c2aef8e682ab8c1
Robert Jesionek <yesionuk@gmail.com>
2016-09-02 22:06:21 +0200

    DI on host

--- src/app/components/host/host.component.ts
+++ src/app/components/host/host.component.ts
@@ -2,36 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { ReactChildComponentView } from '../react/child/child';
 import { ValueStore } from './redux/list.store';
 import { add, remove } from './redux/list.action';
+import { Model } from '../../domain/model';
 
 @Component({
 	selector: 'host-component',
 	templateUrl: 'app/components/host/host.component.html',
-	styleUrls: ['app/components/host/host.component.css']
+	styleUrls: ['app/components/host/host.component.css'],
+	providers: [
+		Model
+	]
 })
 
 export class HostComponent implements OnInit { 
-
-    public helloAngular: string = 'Hello World Angular 2!';
-	public helloReact: string = 'Hello World React!';
 	
 	public store: ValueStore;
 
 	public val: string = 'red';
 	
-	constructor() {
+	constructor( public model: Model ) {
 		this.initStore();			
 	}
 
 	public initStore() {
-		this.store = new ValueStore( ['yellow', 'blue', 'orange', 'black', 'white'] );
+		this.store = new ValueStore( this.model.colors );
 		this.store.store.subscribe(() => {
 			console.log('on state change (angular 2 host): ' + this.store.store.getState().length);
 		});
 	}
 
 	public ngOnInit() {
-		ReactChildComponentView.initialize( this.helloReact, this.store, 'react-component-container' );
-		ReactChildComponentView.initialize( this.helloReact, this.store, 'react-component-container-2' );
+		ReactChildComponentView.initialize( this.model.componentTitle, this.store, 'react-component-container' );
+		ReactChildComponentView.initialize( this.model.componentTitle, this.store, 'react-component-container-2' );
 	}
 
 	public onAdd() {
