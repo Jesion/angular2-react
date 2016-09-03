24381f1bace89aa8de1bdd6414fcdd966eac6dd2
Robert Jesionek <yesionuk@gmail.com>
2016-09-03 21:29:25 +0200

    pulling node uuid

--- src/app/domain/model.ts
+++ src/app/domain/model.ts
@@ -3,10 +3,15 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Model {
 
+    public id: string;
+
     public title: string = 'Hello World Angular 2!';
 
     public componentTitle: string = 'Hello World React!';
 
     public colors: Array<string> = ['yellow', 'blue', 'orange', 'black', 'white'];
 
+    constructor() {
+        this.id = uuid.v4();
+    }
 }
