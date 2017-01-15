141e0b8653334171024364fba73eee5684097363
Robert Jesionek <yesionuk@gmail.com>
2017-01-15 22:13:37 +0100

    tidy up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,14 @@ import {
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HostComponent } from './components/host/host.component';
-import { FormComponent } from './components/form/form.component';
 import { HttpModule } from '@angular/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { routing } from './app.routes';
-import { DateControlComponent } from './components/controls/date.component';
-import { NameControlComponent } from './components/controls/name.component';
 
 @NgModule({
     declarations: [ 
         AppComponent,
-        HostComponent,
-        FormComponent,
-        DateControlComponent,
-        NameControlComponent
+        HostComponent
     ],
     imports: [ 
         routing,
